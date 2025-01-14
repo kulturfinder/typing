a2de02a324cd2d7737bbce2364812c6646bb762e
Tjorben Schiller <tjorben.schiller@dataport.de>
2025-01-14 14:57:02 +0100

    feat: delete not needed templates

--- generate-typing.sh
+++ generate-typing.sh
@@ -3,7 +3,7 @@
 openapi-generator generate \
   -i https://kulturfindertest.dsecurecloud.de/api/swagger/v1/swagger.json \
   -g typescript-fetch \
-  -t ./templates/typescript \
+  -t ./templates/typescript/ \
   -o ./typescript/ \
   --additional-properties=skipApiGeneration=true,supportsES6=true
 
