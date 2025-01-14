076126a9e0ee27470f6bd94bdc0140c54bd593dd
Tjorben Schiller <tjorben.schiller@dataport.de>
2025-01-14 15:40:40 +0100

    feat: update config

--- generate-typing.sh
+++ generate-typing.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-openapi-generator generate -c ./config.yml
+openapi-generator generate \
+ -i https://kulturfindertest.dsecurecloud.de/api/swagger/v1/swagger.json \
+ -g typescript-node \
+ -t ./templates/typescript \
+ -o ./typescript/ \
+ --global-property models \
+ --additional-properties \
+ skipApiGeneration=true,
+ modelOnly=true,
+ supportsES6=true,
+ useSingleRequestParameter=true,
+ withInterfaces=true
